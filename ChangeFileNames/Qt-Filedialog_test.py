6533470dab131ed78a6ba18219362a529c72d986
niklasniklas <niclas@appleby.se>
2016-11-14 20:43:10 +0100

    addItem

--- ChangeFileNames/Qt-Filedialog_test.py
+++ ChangeFileNames/Qt-Filedialog_test.py
@@ -5,7 +5,7 @@ Created on Sat Nov 12 08:42:36 2016
 @author: NiclasLaptop
 """
 
-#from PySide import QtGui
+"""
 from PyQt4 import QtCore, QtGui
 
 app = QtGui.QApplication([])
@@ -13,3 +13,42 @@ dialog = QtGui.QFileDialog()
 dialog.setFileMode(QtGui.QFileDialog.Directory)
 dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
 dialog.exec_()
+"""
+
+"""
+import sys
+from PyQt4.QtGui import *
+app = QApplication(sys.argv)
+listWidget = QListWidget()
+for i in range(10):
+    item = QListWidgetItem("Item %i" % i)
+    listWidget.addItem(item)
+listWidget.show()
+sys.exit(app.exec_())
+"""
+import sys
+from PyQt4 import QtCore, QtGui
+
+class MyWidget(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.button1 = QtGui.QPushButton('Test', self)
+#        self.button1.clicked.connect(self.loadButton)
+        self.button2 = QtGui.QPushButton('2', self)
+#        self.button2.clicked.connect(self.secondButton)
+        self.listWgt = QtGui.QListWidget(self)
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.listWgt)
+        self.listWgt.addItem("AA")
+        
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    myWidgt = MyWidget()
+    myWidgt.show()
+
+    for i in range(5):
+        myWidgt.listWgt.addItem("Hej %d" % i)    
+    sys.exit(app.exec_())
--- ChangeFileNames/changeFilesWgui.py
+++ ChangeFileNames/changeFilesWgui.py
@@ -17,62 +17,73 @@ import shutil
 import sys
 
 
-class FileDialog(QtGui.QFileDialog):
+class PathDialog(QtGui.QFileDialog):
     def __init__(self, *args, **kwargs):
-        super(FileDialog, self).__init__(*args, **kwargs)
+        super(PathDialog, self).__init__(*args, **kwargs)
         self.setFileMode(QtGui.QFileDialog.Directory)
         self.setOption(QtGui.QFileDialog.ShowDirsOnly, True)
-#        self.setOption(QtGui.QFileDialog.DontUseNativeDialog, True)
- #       self.setFileMode(QtGui.QFileDialog.ExistingFiles)
 
     def accept(self):
         QtGui.QDialog.accept(self)
 
+
+
+class FileDialog(QtGui.QFileDialog):
+    def __init__(self, *args, **kwargs):
+        super(FileDialog, self).__init__(*args, **kwargs)
+#        self.setFileMode(QtGui.QFileDialog.Directory)
+#        self.setOption(QtGui.QFileDialog.ShowDirsOnly, True)
+#        self.setOption(QtGui.QFileDialog.DontUseNativeDialog, True)
+        self.setFileMode(QtGui.QFileDialog.ExistingFiles)
+
+    def accept(self):
+        QtGui.QDialog.accept(self)
+
+
 class Window(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
-        self.button = QtGui.QPushButton('Test', self)
-        self.button.clicked.connect(self.loadButton)
-        self.list = QtGui.QListView(self)
+        self.button1 = QtGui.QPushButton('Choose files...', self)
+        self.button1.clicked.connect(self.loadButton)
+        self.button2 = QtGui.QPushButton('Change outputfolder', self)
+        self.button2.clicked.connect(self.secondButton)
+        self.listWgt = QtGui.QListWidget(self)
+        self.labelPath = QtGui.QLabel()
         layout = QtGui.QVBoxLayout(self)
-        layout.addWidget(self.button)
-        layout.addWidget(self.list)
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.listWgt)
+        layout.addWidget(self.labelPath)
+        
+
+    def secondButton(self):
+        dialog= PathDialog()
+        if dialog.exec_() == QtGui.QDialog.Accepted:
+            print dialog.selectFile()[0]
+        self.labelPath.setText("2: Hej")
+        item = QtGui.QListWidgetItem("Aaaa")
+#        self.listWgt.addItem(item)
+        
 
     def loadButton(self):
         dialog = FileDialog()
-        print("Hej")
-        print(dialog.selectedFiles())
-
         if dialog.exec_() == QtGui.QDialog.Accepted:
-#            print(dialog.selectedFiles())
- #           print(dialog.selectedFiles()[0])
-  #          print(dialog.selectedFiles()[1])
-   #         print(dialog.selectedFiles()[2])
-            aa = dialog.selectedFiles()
-            print len(aa)
-            for i in range(0,len(aa)):
-                print dialog.selectedFiles()[i]
-                list.append(i)
-#                list.append(0,dialog.selectedFiles()[i])
-                
-#            for file_i in dialog.selectedFiles:
- #               print file_i
-  #              self.list.append(dialog.selectedFiles()[file_i])
- #               print dialog.selectedFiles()[file_i]
-            
-            print list
+            for i in range(0,len(dialog.selectedFiles())):
+#                print dialog.selectedFiles()[i]
+                item = QtGui.QListWidgetItem(dialog.selectedFiles()[i])
+                self.listWgt.addItem(item)
+
 
 if __name__ == '__main__':
-    print 10
 #    os.mkdir("C:\\xxx\\copy")
 #    shutil.copy("C:\\xxx\\aaa.jpg","C:\\xxx\\copy\\cccc.jpg")
 #    shutil.move(os.path.join(path,files),newfile)
-    n = 101
-    sum = 0
-    for i in range(1,n):
-        print i
-        sum = sum + i
-    print sum
+#    n = 101
+#    sum = 0
+#    for i in range(1,n):
+#        print i
+#        sum = sum + i
+#    print sum
 
 #    import sys
     app = QtGui.QApplication(sys.argv)
